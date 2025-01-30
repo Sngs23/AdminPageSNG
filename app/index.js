8dcf7a0645c0687c3f25649a6426ff8e704a1175
Sngs23 <silvionazarego99@gmail.com>
2025-01-30 14:01:02 -0300

    Primer commit, pagina completa

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import  path from 'path';
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { methods as authentication } from "./controllers/authenticationController";
+//Server 
+const app = express();
+
+app.set("port",4000);
+app.listen(app.get("port"));
+console.log("server en puerto: ",app.get("port"));
+
+//Configuracion
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+//Rutas
+app.get("/",(req,res)=> res.sendFile( __dirname + "/pages/login.html"));
+app.get("/register",(req,res)=> res.sendFile( __dirname + "/pages/register.html"));
+app.get("/admin",(req,res)=> res.sendFile( __dirname + "/pages/admin/admin.html"));
+app.get("/api/register",authentication.register);
+app.get("/api/login",authentication.login);
